c1273e975fcc808293bc4087b4b6f508f44b89dd
VoidArc Studio <chudybyczek2137@gmail.com>
2025-10-06 15:29:26 +0200

    Update remove.sh

--- zcr-build-files/remove.sh
+++ zcr-build-files/remove.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-sudo rm -rf /usr/share/applications/sample-zcr-repo.desktop
-sudo rm -rf /usr/lib/zcr/sample-zcr-repo/
-sudo rm -rf /usr/bin/sample-zcr-repo
+sudo rm -rf /usr/share/applications/nula-lang.desktop
+sudo rm -rf /usr/lib/zcr/Nula-ZCR/
+sudo rm -rf ~/.nula/
+sudo rm -rf /usr/bin/nula
