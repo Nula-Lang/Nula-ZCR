fc66bdb0151635cc5d1c0625dda4ff35520c328b
VoidArc Studio <chudybyczek2137@gmail.com>
2025-10-06 15:37:26 +0200

    Update unpack.sh

--- zcr-build-files/unpack.sh
+++ zcr-build-files/unpack.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 sudo mv /usr/lib/zcr/Nula-ZCR/nula-lang.desktop /usr/share/applications/
-sudo mv /usr/lib/zcr/Nula-ZCR/ /usr/bin/
 cd ~
 sudo mkdir .nula
+sudo curl -L https://github.com/Nula-Lang/Nula/releases/download/v0.5/nula -o /usr/bin/nula
+curl -L https://github.com/Nula-Lang/Nula/releases/download/v0.5/nula-go -o ~/.nula/lib/nula-go
+curl -L https://github.com/Nula-Lang/Nula/releases/download/v0.5/nula-zig -o ~/.nula/lib/nula-zig
 sudo chmod a+x /usr/bin/nula
 sudo chmod a+x ~/.nula/lib/nula-go
 sudo chmod a+x ~/.nula/lib/nula-zig
