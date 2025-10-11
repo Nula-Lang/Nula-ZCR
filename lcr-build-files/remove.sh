c26c05c276887590b4cc4f466cc5b9f7c9bad39b
michal92299 <chudybyczek2137@gmail.com>
2025-10-11 17:26:31 +0200

    Update and rename remove.sh to remove.sh

--- lcr-build-files/remove.sh
+++ lcr-build-files/remove.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo rm -rf /usr/share/applications/nula-lang.desktop
-sudo rm -rf /usr/lib/zcr/Nula-ZCR/
+sudo rm -rf /usr/lib/lcr/Nula-LCR/
 sudo rm -rf ~/.nula/
 sudo rm -rf /usr/bin/nula
